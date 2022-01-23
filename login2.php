75e72fec14eb8d555b36453044dd77383e662260
GuoJiaSetsuna <93181404+GuoJiaSetsuna@users.noreply.github.com>
2022-01-24 03:45:39 +0800

    Update login2.php

--- login2.php
+++ login2.php
@@ -71,7 +71,7 @@
 												$account = addslashes($account);
 												$password = addslashes($password);
 												// 將輸入的帳號密碼使用SQL語法編譯並放入變數
-												$query = "SELECT * FROM public.user where account like '".$account."';";
+												$query = "SELECT ID,account,convert_from(decrypt(password,'My Secret Key','aes'), 'utf-8') FROM public.user where account like '".$account."';";
 												// 將SQL語法執行並把結果放入陣列
 												$result = $link->query($query);
 												foreach ($result as $row);
